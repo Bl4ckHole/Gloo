3471dc99c1b862271b9160726332d52dbf2f4baa
Bl4ckHole <corentinja@gmail.com>
2016-03-22 15:18:50 +0100

    Added basic Gloo script and utils script

--- Assets/Scripts/glooScript.cs
+++ Assets/Scripts/glooScript.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Utils;
 
 public class glooScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private Animator animator;
+    private Rigidbody2D rbody;
+    private float speed = 1.5f;
+    private int hashLeft = Animator.StringToHash("run_left");
+    private int hashRight = Animator.StringToHash("run_right");
+    private int hashJumpL = Animator.StringToHash("jump_left");
+    private int hashJumpR = Animator.StringToHash("jump_right");
+    bool inJump = false;
+
+    // Use this for initialization
+    void Start() {
+        animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody2D>();
+    }        
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        bool right = Input.GetKey(GlooConstants.keyRight);
+        bool left = Input.GetKey(GlooConstants.keyLeft);
+        animator.SetBool("GoLeft", left);
+        animator.SetBool("GoRight", right && !left);
+        animator.SetBool("Jump", inJump);
+    }
+
+    void FixedUpdate() {
+        Vector2 move = new Vector2(0, 0);
+        int currentHash = animator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+        if (currentHash == hashLeft || (currentHash == hashJumpL && Input.GetKey(GlooConstants.keyLeft))) {
+            move += new Vector2(-1, 0);
+        }
+        if (currentHash == hashRight || (currentHash == hashJumpR && Input.GetKey(GlooConstants.keyRight))) {
+            move += new Vector2(1, 0);
+        }
+        if (Input.GetKey(GlooConstants.keyJump) && !inJump) {
+            rbody.AddForce(new Vector2(0, 3.5f), ForceMode2D.Impulse);
+            inJump = true;
+        }
+        move *= speed;
+        float vy = rbody.velocity.y;
+        rbody.velocity = move + new Vector2((inJump && move.x == 0) ? rbody.velocity.x : 0, vy);
+    }
+
+    void OnCollisionStay2D(Collision2D coll) {
+        if (coll.relativeVelocity.y < 0.16) {
+            inJump = false;
+        }
+    }
 }
---
+++ Assets/Scripts/utils.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Utils{
+    
+    public static class GlooConstants {
+        public static KeyCode keyUp = KeyCode.Z;
+        public static KeyCode keyDown = KeyCode.S;
+        public static KeyCode keyLeft = KeyCode.Q;
+        public static KeyCode keyRight = KeyCode.D;
+        public static KeyCode keyDivide = KeyCode.E;
+        public static KeyCode keyJump = KeyCode.Space;
+    }
+}
