2013b30388e1481343475d64d449aa69c87c45b2
mmmbao <bao-vi.defaux@depinfonancy.net>
2016-05-10 16:39:54 +0200

    [Level1] Activation du pont

---
+++ Assets/Scripts/ActivableObjectClass.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivableObjectClass : MonoBehaviour {
+
+	public Animation animationName;
+	private GameObject gameObject;
+	private EdgeCollider2D limite;
+	private SpriteRenderer image;
+
+	// Use this for initialization
+	void Start () {
+		gameObject = GameObject.Find("pont");
+		limite = GetComponent<EdgeCollider2D> ();
+		limite.enabled = false;
+		image = GetComponent<SpriteRenderer> ();
+		image.enabled = false;
+	}
+	
+	// Update is called once per frame
+	public void Activate (bool isActivated) {
+		limite.enabled = isActivated;
+		//animationName.Play ();
+		image.enabled = isActivated;
+	}
+}
