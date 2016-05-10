2d8da27e4c36dd36c19bf4041e4ea9bb8ad29a47
Xousi <alexispetitjean@hotmail.fr>
2016-05-10 16:49:07 +0200

    ADD Rush enemi and Enemi class

---
+++ Assets/Scripts/OstileClass.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class OstileClass : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionStay2D(Collision2D coll) 
+	{
+		if (coll.gameObject.tag=="Gloo" || coll.gameObject.tag=="GlooDiv")
+		{
+			coll.gameObject.SendMessage ("Die");
+		}
+
+	}
+}
---
+++ Assets/Scripts/RushScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RushScript : OstileClass 
+{
+	private int distanceDeDetection = 20;
+	private Rigidbody2D body;
+	private BoxCollider2D boxcoll;
+	private float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		base.Start ();
+		boxcoll = GetComponent<BoxCollider2D> ();
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+//		Physics2D.Raycast ();
+//		body.velocity =  + new Vector2((data.inJump && move.x == 0) ? rbody.velocity.x : 0, vy);
+	}
+}
