549807a208cf0a019e5bb806e54b731b51528c2c
Xousi <alexispetitjean@hotmail.fr>
2016-05-30 12:35:41 +0200

    world (and rocks) are now saved at checkpoint

---
+++ Assets/Scripts/PositionToSaveScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionToSaveScript : MonoBehaviour, GlooGenericObject {
+
+    private Rigidbody2D body;
+
+
+    private class RushData
+    {
+        public Vector2 position;
+
+        public RushData(Vector3 pos)
+        {
+            position = pos;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public object getData()
+    {
+        return new RushData(this.transform.position);
+    }
+
+    public void setData(object savedData)
+    {
+        RushData data = savedData as RushData;
+        this.transform.position = data.position;
+    }
+
+}
