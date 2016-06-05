cfc6877095f50e08677c799b9d618057169f5579
JulienOuryNogues <julien.oury-nogues@depinfonancy.net>
2016-06-05 17:24:11 +0200

    Mouvement de caméra
    Avec les flèches directionnelles

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -10,41 +10,71 @@ public class CameraBehavior : MonoBehaviour
     public GameObject target;
     public string currenttarget;
     Vector3 targetPos;
+    public float speed;
     // Use this for initialization
     void Start()
     {
         targetPos = transform.position;
         currenttarget = "Gloo";
         distanceMin = 1.0f;
+        speed = 0.15f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        target = GameObject.Find(currenttarget);
-        Vector3 posNoZ = transform.position;
-        posNoZ.z = target.transform.position.z;
+        Vector3 move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow) | Input.GetKey(KeyCode.DownArrow) | Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.LeftArrow)) {
 
-        Vector3 targetDirection = (target.transform.position - posNoZ);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                move += new Vector3(0, 1, 0);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                move += new Vector3(0, -1, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                move += new Vector3(1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                move += new Vector3(-1, 0, 0);
+            }
+            move *= speed;
 
-        interpVelocity = targetDirection.magnitude * 5f;
-
-        targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
-
-        Vector3 resultat = Vector3.Lerp(transform.position, targetPos, 0.25f);
-
-        Vector3 Id= Vector3.Lerp(transform.position, transform.position, 0.25f);
-
-        if (Mathf.Abs(targetDirection.x) <0.5f) {
-            resultat.x = Id.x;
         }
-        if (Mathf.Abs(targetDirection.y)<2.0f) {
-            resultat.y = Id.y;
+        
+        else {
+            target = GameObject.Find(currenttarget);
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+
+            interpVelocity = targetDirection.magnitude * 5f;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+            Vector3 resultat = Vector3.Lerp(transform.position, targetPos, 0.25f);
+
+            Vector3 Id = Vector3.Lerp(transform.position, transform.position, 0.25f);
+
+            if (Mathf.Abs(targetDirection.x) < 0.5f)
+            {
+                resultat.x = Id.x;
+            }
+            if (Mathf.Abs(targetDirection.y) < 2.0f)
+            {
+                resultat.y = Id.y;
+            }
+
+            transform.position = resultat;
         }
+        transform.position += move;
 
-        transform.position = resultat;
-
-    }
+        }
 
 
     void setCurrentTarget(string nom) {
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,29 +10,48 @@ public class FollowCamera : MonoBehaviour
     public GameObject target;
     public Vector3 offset;
     Vector3 targetPos;
+    public float speed;
     // Use this for initialization
     void Start()
     {
         targetPos = transform.position;
+        speed = 0.5f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector3 move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            move += new Vector3(0, 1 ,0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            move += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move += new Vector3(-1, 0 ,0 );
+        }
+        else {
 
-        //target = GameObject.Find("Gloo");
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
 
-        Vector3 posNoZ = transform.position;
-        posNoZ.z = target.transform.position.z;
+            Vector3 targetDirection = (target.transform.position - posNoZ);
 
-        Vector3 targetDirection = (target.transform.position - posNoZ);
+            interpVelocity = targetDirection.magnitude * 5f;
 
-        interpVelocity = targetDirection.magnitude * 5f;
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
 
-        targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+        }
 
-        transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
-
+        move *= speed;
+        transform.position += move;
 
     }
 }
