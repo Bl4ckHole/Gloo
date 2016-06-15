b42f6607fc785593f99a0685f9686db41fac1135
mmmbao <bao-vi.defaux@depinfonancy.net>
2016-06-15 15:44:15 +0200

    Arrivée

---
+++ Assets/Scripts/NextLevelScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextLevelScript : MonoBehaviour {
+
+	public string nextLevel;
+	private GameObject gloo;
+	private bool[] divOk;
+	private int compteur;
+	public int divNeeded;
+
+	// Use this for initialization
+	void Start () {
+		gloo = GameObject.Find ("Gloo");
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Gloo") {
+			divOk = gloo.GetComponent<glooScript> ().getDivInGloo ();
+			compteur = 0;
+			foreach (bool i in divOk) {
+				if (i) {
+					compteur += 1;
+				}
+			}
+			if (compteur == divNeeded) {
+				SceneManager.LoadScene(nextLevel);
+			}
+		}
+	}
+}
