1b7b7710296d53f9418e1e5a468f12f28c69bef0
JulienOuryNogues <julien.oury-nogues@depinfonancy.net>
2016-06-20 21:08:00 +0200

    Oups

--- Assets/Scripts/glooScript.cs
+++ Assets/Scripts/glooScript.cs
@@ -133,7 +133,7 @@ public class glooScript : MonoBehaviour, GlooGenericObject {
 
     // Update is called once per frame
     void Update () {
-        sphereAbsorption = GameObject.Find("sphereAbsorption");
+        //sphereAbsorption = GameObject.Find("sphereAbsorption");
 
         if (updateDivInGlooRequest)
         {
