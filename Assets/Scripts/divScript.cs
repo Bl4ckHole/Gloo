063ebcf09e499b05558b45aadc41950b82c05d6f
JulienOuryNogues <julien.oury-nogues@depinfonancy.net>
2016-06-20 21:31:35 +0200

    Betise

--- Assets/Scripts/divScript.cs
+++ Assets/Scripts/divScript.cs
@@ -145,7 +145,7 @@ public class divScript : MonoBehaviour, GlooGenericObject {
             }
         }
 
-        oldJump = false;
+        oldJump = jump;
         Vector2 move = new Vector2(0, 0);
         if (left && canMoveLeft) {
             
